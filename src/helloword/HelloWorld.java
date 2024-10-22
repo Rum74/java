0fbb732db80a2df030accfff5c865ac07895b88a
Huỳnh Văn Phòng <huynhvanphong7402@gmail.com>
2024-10-22 16:58:07 +0700

    first commit

---
+++ src/helloword/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloword;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with Huynh Van Phong");
+		System.out.println("Hello World with Huynh Van Phong");
+		System.out.println("Hello World with Huynh Van Phong");
+		System.out.println("Hello World with Huynh Van Phong");
+	}
+}
